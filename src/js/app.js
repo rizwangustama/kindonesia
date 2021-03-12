41c27ae37fa5193bed16014f3d795e9441ba6e3d
Rizwan Gustama <50127801+rizwangustama@users.noreply.github.com>
2021-03-12 08:28:34 +0700

    font-end

---
+++ src/js/app.js
@@ -0,0 +1,23 @@
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    // init: false,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 2,
+            spaceBetween: 50,
+        },
+    }
+});
